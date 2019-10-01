8bb521f34afb6abdb740eb6cbbf99df39607b17f
Emily Yang <eyang42014@gmail.com>
2019-10-01 13:13:20 -0400

    commit

---
+++ myscript.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    const mainImg = document.getElementById("main-img"); //why doesn't this work when it is in called through getElementByClass
+
+    const imagesBun = document.querySelector(".product-image-indicators");
+
+    imagesBun.addEventListener(
+        "click", 
+        ev =>{
+            if (ev.target.tagName.toUpperCase() === "IMG"){
+                mainImg.src = ev.target.src
+            }
+        }
+
+    );
+
+})
